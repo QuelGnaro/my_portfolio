b96746cd92cb9a9fb743ed941be225669bd90c64
QuelGnaro <144672084+QuelGnaro@users.noreply.github.com>
2024-02-17 15:12:57 +0100

    skills added fixed glass card

--- src/app/pages/baratti-lorenzo/pages/home/baratti-lorenzo.component.ts
+++ src/app/pages/baratti-lorenzo/pages/home/baratti-lorenzo.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./baratti-lorenzo.component.scss']
 })
 export class BarattiLorenzoComponent {
-
+  skillsArray = [
+    {
+      name: 'Angular',
+      url: 'https://www.svgrepo.com/show/373427/angular.svg',
+      description: ''
+    },
+    {
+      name: 'HTML',
+      url: 'https://www.svgrepo.com/show/452228/html-5.svg',
+      description: ''
+    },
+    {
+      name: 'JavaScript',
+      url: 'https://www.svgrepo.com/show/452045/js.svg',
+      description: ''
+    },
+    {
+      name: 'TypeScript',
+      url: 'https://www.svgrepo.com/show/374146/typescript-official.svg',
+      description: ''
+    },
+    {
+      name: 'Electron Framework',
+      url: 'https://www.svgrepo.com/show/353689/electron.svg',
+      description: ''
+    },
+    {
+      name: 'Ionic',
+      url: 'https://www.svgrepo.com/show/373687/ionic.svg',
+      description: ''
+    },
+    {
+      name: 'CSS',
+      url: 'https://www.svgrepo.com/show/452185/css-3.svg',
+      description: ''
+    },
+    {
+      name: 'GitHub',
+      url: 'https://www.svgrepo.com/show/450156/github.svg',
+      description: ''
+    },
+  ];
 }
