5c8d36975f093c4fe87c80912b16361d3752050a
QuelGnaro <144672084+QuelGnaro@users.noreply.github.com>
2024-02-20 17:33:53 +0100

    commento menu

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -65,58 +65,58 @@ export class AppMenuComponent implements OnInit {
             //         { label: 'PrimeFlex', icon: 'pi pi-fw pi-desktop', url: ['https://www.primefaces.org/primeflex/'], target: '_blank' },
             //     ]
             // },
-            {
-                label: 'Pages',
-                icon: 'pi pi-fw pi-briefcase',
-                items: [
-                    {
-                        label: 'Landing',
-                        icon: 'pi pi-fw pi-globe',
-                        routerLink: ['/landing']
-                    },
-                    {
-                        label: 'Auth',
-                        icon: 'pi pi-fw pi-user',
-                        items: [
-                            {
-                                label: 'Login',
-                                icon: 'pi pi-fw pi-sign-in',
-                                routerLink: ['/auth/login']
-                            },
-                            {
-                                label: 'Error',
-                                icon: 'pi pi-fw pi-times-circle',
-                                routerLink: ['/auth/error']
-                            },
-                            {
-                                label: 'Access Denied',
-                                icon: 'pi pi-fw pi-lock',
-                                routerLink: ['/auth/access']
-                            }
-                        ]
-                    },
-                    {
-                        label: 'Crud',
-                        icon: 'pi pi-fw pi-pencil',
-                        routerLink: ['/pages/crud']
-                    },
-                    {
-                        label: 'Timeline',
-                        icon: 'pi pi-fw pi-calendar',
-                        routerLink: ['/pages/timeline']
-                    },
-                    {
-                        label: 'Not Found',
-                        icon: 'pi pi-fw pi-exclamation-circle',
-                        routerLink: ['/notfound']
-                    },
-                    {
-                        label: 'Empty',
-                        icon: 'pi pi-fw pi-circle-off',
-                        routerLink: ['/pages/empty']
-                    },
-                ]
-            },
+            // {
+            //     label: 'Pages',
+            //     icon: 'pi pi-fw pi-briefcase',
+            //     items: [
+            //         {
+            //             label: 'Landing',
+            //             icon: 'pi pi-fw pi-globe',
+            //             routerLink: ['/landing']
+            //         },
+            //         {
+            //             label: 'Auth',
+            //             icon: 'pi pi-fw pi-user',
+            //             items: [
+            //                 {
+            //                     label: 'Login',
+            //                     icon: 'pi pi-fw pi-sign-in',
+            //                     routerLink: ['/auth/login']
+            //                 },
+            //                 {
+            //                     label: 'Error',
+            //                     icon: 'pi pi-fw pi-times-circle',
+            //                     routerLink: ['/auth/error']
+            //                 },
+            //                 {
+            //                     label: 'Access Denied',
+            //                     icon: 'pi pi-fw pi-lock',
+            //                     routerLink: ['/auth/access']
+            //                 }
+            //             ]
+            //         },
+            //         {
+            //             label: 'Crud',
+            //             icon: 'pi pi-fw pi-pencil',
+            //             routerLink: ['/pages/crud']
+            //         },
+            //         {
+            //             label: 'Timeline',
+            //             icon: 'pi pi-fw pi-calendar',
+            //             routerLink: ['/pages/timeline']
+            //         },
+            //         {
+            //             label: 'Not Found',
+            //             icon: 'pi pi-fw pi-exclamation-circle',
+            //             routerLink: ['/notfound']
+            //         },
+            //         {
+            //             label: 'Empty',
+            //             icon: 'pi pi-fw pi-circle-off',
+            //             routerLink: ['/pages/empty']
+            //         },
+            //     ]
+            // },
             // {
             //     label: 'Hierarchy',
             //     items: [
